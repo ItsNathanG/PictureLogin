d7006fa170832976100418981d21f4ccb36cf204
Nathan Glover <contact@itsnathang.me>
2018-02-16 13:06:21 -0500

    Added Metrics!

--- src/main/java/me/itsnathang/picturelogin/PictureLogin.java
+++ src/main/java/me/itsnathang/picturelogin/PictureLogin.java
@@ -6,6 +6,7 @@ import me.itsnathang.picturelogin.commands.BaseCommand;
 import me.itsnathang.picturelogin.config.ConfigManager;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.plugin.java.JavaPlugin;
+import org.bstats.bukkit.MetricsLite;
 
 import me.itsnathang.picturelogin.util.PictureUtil;
 
@@ -13,12 +14,16 @@ import me.itsnathang.picturelogin.util.PictureUtil;
 public class PictureLogin extends JavaPlugin implements CommandExecutor {
 	@Override
 	public void onEnable() {
+
 		getServer().getPluginManager().registerEvents(new JoinListener(this), this);
 		getServer().getPluginManager().registerEvents(new QuitListener(this), this);
 		getCommand("picturelogin").setExecutor(new BaseCommand(this));
 		
 		new PictureUtil(this);
 		new ConfigManager(this);
+
+		// bStats integration
+		new MetricsLite(this);
 	}
 	
 	@Override

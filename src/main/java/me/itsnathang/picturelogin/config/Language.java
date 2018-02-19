dffaa0ef1ec3fcf08a52305d9f0cc60559071377
Nathan Glover <contact@itsnathang.me>
2018-02-19 00:07:39 -0500

    Added language support!

---
+++ src/main/java/me/itsnathang/picturelogin/config/Language.java
@@ -0,0 +1,40 @@
+package me.itsnathang.picturelogin.config;
+
+import me.itsnathang.picturelogin.PictureLogin;
+import org.bukkit.ChatColor;
+import org.bukkit.configuration.file.YamlConfiguration;
+
+import java.io.File;
+
+public class Language {
+    private static YamlConfiguration messages;
+    private static PictureLogin plugin;
+
+    public Language(PictureLogin pl) {
+        plugin = pl;
+
+        messages = loadLanguage();
+    }
+
+    private static YamlConfiguration loadLanguage() {
+        File language_file = new File(plugin.getDataFolder() + File.separator + "messages.yml");
+
+        if (!language_file.exists())
+            plugin.saveResource("messages.yml", false);
+
+        return YamlConfiguration.loadConfiguration(language_file);
+    }
+
+    public static void reloadLanguage() { messages = loadLanguage(); }
+
+    public static String tl(String key) {
+        return color(messages.getString(key)
+                .replace("%prefix%", messages.getString("prefix"))
+                .replace("%new_line%", "\n"));
+    }
+
+    private static String color(String message) {
+        return ChatColor.translateAlternateColorCodes('&', message);
+    }
+
+}

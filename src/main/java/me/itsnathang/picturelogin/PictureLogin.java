ba145aaca8b809fd4a7dc24886cf71c19ef18b08
Nick <nvoss@me.com>
2022-04-07 16:48:03 -0500

    Working on Minecraft 1.18.2

--- src/main/java/me/itsnathang/picturelogin/PictureLogin.java
+++ src/main/java/me/itsnathang/picturelogin/PictureLogin.java
@@ -8,49 +8,48 @@ import me.itsnathang.picturelogin.util.Hooks;
 import me.itsnathang.picturelogin.util.PictureUtil;
 
 import me.itsnathang.picturelogin.util.Updater;
-import org.bukkit.command.CommandExecutor;
 import org.bukkit.plugin.java.JavaPlugin;
 import org.bstats.bukkit.Metrics;
 
 public class PictureLogin extends JavaPlugin {
-	private ConfigManager configManager;
-	private PictureUtil pictureUtil;
+    private ConfigManager configManager;
+    private PictureUtil pictureUtil;
 
-	@Override
-	public void onEnable() {
-		// load config & languages file
-		configManager = new ConfigManager(this);
+    @Override
+    public void onEnable() {
+        // load config & languages file
+        configManager = new ConfigManager(this);
 
-		// Register Plugin Hooks
-		new Hooks(getServer().getPluginManager(), configManager, getLogger());
+        // Register Plugin Hooks
+        new Hooks(getServer().getPluginManager(), configManager, getLogger());
 
-		// Initialize Picture Utility
-		pictureUtil = new PictureUtil(this);
+        // Initialize Picture Utility
+        pictureUtil = new PictureUtil(this);
 
-		// register Listeners
-		getServer().getPluginManager().registerEvents(new JoinListener(this), this);
+        // register Listeners
+        getServer().getPluginManager().registerEvents(new JoinListener(this), this);
 
-		// (only register leave listener if enabled in config)
-		if (configManager.getBoolean("show-leave-message", false))
-			getServer().getPluginManager().registerEvents(new QuitListener(this), this);
+        // (only register leave listener if enabled in config)
+        if (configManager.getBoolean("show-leave-message", false))
+            getServer().getPluginManager().registerEvents(new QuitListener(this), this);
 
-		// register /picturelogin command
-		getCommand("picturelogin").setExecutor(new BaseCommand(this));
+        // register /picturelogin command
+        getCommand("picturelogin").setExecutor(new BaseCommand(this));
 
-		// Update Checker
-		if (configManager.getBoolean("update-check", true))
-			new Updater(getLogger(), getDescription().getVersion());
+        // Update Checker
+        if (configManager.getBoolean("update-check", true))
+            new Updater(getLogger(), getDescription().getVersion());
 
-		// bStats integration
-		if (configManager.getBoolean("metrics", true))
-			new Metrics(this, 2225);
-	}
+        // bStats integration
+        if (configManager.getBoolean("metrics", true))
+            new Metrics(this, 2225);
+    }
 
-	public ConfigManager getConfigManager() {
-		return configManager;
-	}
+    public ConfigManager getConfigManager() {
+        return configManager;
+    }
 
-	public PictureUtil getPictureUtil() {
-		return pictureUtil;
-	}
+    public PictureUtil getPictureUtil() {
+        return pictureUtil;
+    }
 }
